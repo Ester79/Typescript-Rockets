d601b1f4b3938fea0e2f8461246aff78c6793471
Ester79 <esterramoscuervo@hotmail.com>
2021-05-20 10:56:07 +0200

    Optimizacion codigo Nivel 2 y 3

--- Nivel_2 y 3/controllers/controlador.js
+++ Nivel_2 y 3/controllers/controlador.js
@@ -4,23 +4,23 @@
 var potencias1 = [10, 30, 80];
 var potencias2 = [30, 40, 50, 50, 30, 10];
 // se tienen dos variables de clase Cohete, una para cada uno de los dos cohetes 
+var cohete0;
 var cohete1;
-var cohete2;
 // se tiene otra variable de tipo array en la que se subirá y almacenará la información de los cohetes que se vayan creando
 var cohetes = new Array(); //infinitos cohetes
 /****************************INTERACCIONES QUE PUEDE HACER EL USUARIO CON LA INTERFAZ*********************/
 // CREAR COHETES. En este caso se han definido los dos cohetes predeterminados
 function crearCohete1() {
-    cohete1 = new Cohete('32WESSDS', potencias1); //parámetros que recibe la variable 'cohete1' de la clase 'Cohete'
-    console.log(cohete1);
-    cohetes.push(cohete1); //subir cohete1 al array de cohetes
+    cohete0 = new Cohete('32WESSDS', potencias1); //parámetros que recibe la variable 'cohete1' de la clase 'Cohete'
+    console.log(cohete0);
+    cohetes[0] = cohete0; //subir cohete1 al array de cohetes
     var CR1 = document.getElementById("CodR1");
     if (CR1) {
-        CR1.innerHTML = cohete1.codigo;
+        CR1.innerHTML = cohete0.codigo;
     }
     var MaxPW1 = document.getElementById("MaxPWR1");
     if (MaxPW1) {
-        MaxPW1.innerHTML = cohete1.potenciaTotalMaxima();
+        MaxPW1.innerHTML = cohete0.potenciaTotalMaxima();
     }
     var boosters1 = document.getElementById("boostersR1");
     if (boosters1) {
@@ -28,16 +28,16 @@ function crearCohete1() {
     }
 }
 function crearCohete2() {
-    cohete2 = new Cohete('LDSFJA32', potencias2);
-    console.log(cohete2);
-    cohetes.push(cohete2); //subir cohete al array de cohetes
+    cohete1 = new Cohete('LDSFJA32', potencias2);
+    console.log(cohete1);
+    cohetes[1] = cohete1; //subir cohete al array de cohetes
     var CR2 = document.getElementById("CodR2");
     if (CR2) {
-        CR2.innerHTML = cohete2.codigo;
+        CR2.innerHTML = cohete1.codigo;
     }
     var MaxPW2 = document.getElementById("MaxPWR2");
     if (MaxPW2) {
-        MaxPW2.innerHTML = cohete2.potenciaTotalMaxima();
+        MaxPW2.innerHTML = cohete1.potenciaTotalMaxima();
     }
     var boosters2 = document.getElementById("boostersR2");
     if (boosters2) {
@@ -45,38 +45,22 @@ function crearCohete2() {
     }
 }
 // ACCELERAR COHETES. 
-function accelera1() {
-    cohete1.accelerar(); //el cohete1 llama al método 'accelerar' de la clase 'Cohete'
-    imprime1(); // llama a la función para imprimir por pantalla los datos
-    advertenciaR1();
-}
-function accelera2() {
-    cohete2.accelerar();
-    imprime2();
-    advertenciaR2();
+function accelera(id) {
+    cohetes[id].accelerar(); //el cohete1 llama al método 'accelerar' de la clase 'Cohete'
+    imprime(id); // llama a la función para imprimir por pantalla los datos
+    advertencia(id);
 }
 // FRENAR COHETES. Misma dinámica que 'accelerar' pero llamando al métod 'frenar' de la clase 'Cohete'
-function frena1() {
-    cohete1.frenar();
-    imprime1();
-    advertenciaR1();
-}
-function frena2() {
-    cohete2.frenar();
-    imprime2();
-    advertenciaR2();
+function frena(id) {
+    cohetes[id].frenar();
+    imprime(id);
+    advertencia(id);
 }
 // MOSTRAR DATOS POR PANTALLA. 
-function imprime1() {
-    var info = document.getElementById("infoR1");
+function imprime(id) {
+    var info = document.getElementById("infoR" + id);
     if (info) {
-        info.innerHTML = cohete1.mostrarInfo();
-    }
-}
-function imprime2() {
-    var info = document.getElementById("infoR2");
-    if (info) {
-        info.innerHTML = cohete2.mostrarInfo();
+        info.innerHTML = cohetes[id].mostrarInfo();
     }
 }
 function imprimirTodos() {
@@ -91,42 +75,14 @@ function imprimirTodos() {
 function refreshTodos() {
     var refresh = document.getElementById("todos");
     if (refresh) {
-        refresh.innerHTML = cohete1.mostrarInfo() + "<br><br>" + cohete2.mostrarInfo();
+        refresh.innerHTML = cohete0.mostrarInfo() + "<br><br>" + cohete1.mostrarInfo();
     }
 }
 // MOSTRAR AVISOS
-function advertenciaR1() {
-    var mensaje = document.getElementById("avisoR1");
-    var maxima = cohete1.potenciaTotalMaxima();
-    var actual = cohete1.potenciaTotalActual();
-    var control = maxima - 20;
-    if (mensaje) {
-        mensaje.classList.remove("send");
-        mensaje.classList.remove("stop");
-        mensaje.classList.remove("ready");
-        mensaje.classList.remove("control");
-        if (actual == maxima) {
-            mensaje.innerHTML = "SENDING INTO SPACE!!";
-            mensaje.classList.add("send");
-        }
-        else if (actual >= control) {
-            mensaje.innerHTML = "Ready to send";
-            mensaje.classList.add("ready");
-        }
-        else if (actual == 0) {
-            mensaje.innerHTML = "STOPPED";
-            mensaje.classList.add("stop");
-        }
-        else {
-            mensaje.innerHTML = "Under control";
-            mensaje.classList.add("control");
-        }
-    }
-}
-function advertenciaR2() {
-    var mensaje = document.getElementById("avisoR2");
-    var maxima = cohete2.potenciaTotalMaxima();
-    var actual = cohete2.potenciaTotalActual();
+function advertencia(id) {
+    var mensaje = document.getElementById("avisoR" + id);
+    var maxima = cohetes[id].potenciaTotalMaxima();
+    var actual = cohetes[id].potenciaTotalActual();
     var control = maxima - 20;
     if (mensaje) {
         mensaje.classList.remove("send");
--- Nivel_2 y 3/controllers/controlador.ts
+++ Nivel_2 y 3/controllers/controlador.ts
@@ -6,8 +6,8 @@ var potencias1 = [10, 30, 80];
 var potencias2 = [30, 40, 50, 50, 30, 10];
 
 // se tienen dos variables de clase Cohete, una para cada uno de los dos cohetes 
+var cohete0: Cohete;
 var cohete1: Cohete;
-var cohete2: Cohete;
 // se tiene otra variable de tipo array en la que se subirá y almacenará la información de los cohetes que se vayan creando
 var cohetes: Cohete[] = new Array(); //infinitos cohetes
 
@@ -17,16 +17,16 @@ var cohetes: Cohete[] = new Array(); //infinitos cohetes
 
 // CREAR COHETES. En este caso se han definido los dos cohetes predeterminados
 function crearCohete1() {
-    cohete1 = new Cohete('32WESSDS', potencias1); //parámetros que recibe la variable 'cohete1' de la clase 'Cohete'
-    console.log(cohete1);
-    cohetes.push(cohete1); //subir cohete1 al array de cohetes
+    cohete0 = new Cohete('32WESSDS', potencias1); //parámetros que recibe la variable 'cohete1' de la clase 'Cohete'
+    console.log(cohete0);
+    cohetes[0]=cohete0; //subir cohete1 al array de cohetes
     let CR1 = document.getElementById("CodR1");
     if(CR1){
-        CR1.innerHTML = cohete1.codigo;
+        CR1.innerHTML = cohete0.codigo;
     }
     let MaxPW1:any = document.getElementById("MaxPWR1");
     if(MaxPW1){
-        MaxPW1.innerHTML = cohete1.potenciaTotalMaxima();
+        MaxPW1.innerHTML = cohete0.potenciaTotalMaxima();
     }  
     let boosters1:any = document.getElementById("boostersR1");
     if(boosters1){
@@ -35,16 +35,16 @@ function crearCohete1() {
 }
 
 function crearCohete2() {
-    cohete2 = new Cohete('LDSFJA32', potencias2);
-    console.log(cohete2);
-    cohetes.push(cohete2); //subir cohete al array de cohetes
+    cohete1 = new Cohete('LDSFJA32', potencias2);
+    console.log(cohete1);
+    cohetes[1]=cohete1; //subir cohete al array de cohetes
     let CR2 = document.getElementById("CodR2");
     if(CR2){
-        CR2.innerHTML = cohete2.codigo;
+        CR2.innerHTML = cohete1.codigo;
     }
     let MaxPW2:any = document.getElementById("MaxPWR2");
     if(MaxPW2){
-        MaxPW2.innerHTML = cohete2.potenciaTotalMaxima();
+        MaxPW2.innerHTML = cohete1.potenciaTotalMaxima();
     }  
     let boosters2:any = document.getElementById("boostersR2");
     if(boosters2){
@@ -54,47 +54,33 @@ function crearCohete2() {
 
 
 // ACCELERAR COHETES. 
-function accelera1() {
-    cohete1.accelerar(); //el cohete1 llama al método 'accelerar' de la clase 'Cohete'
-    imprime1(); // llama a la función para imprimir por pantalla los datos
-    advertenciaR1();
+
+function accelera(id:number) {
+    cohetes[id].accelerar(); //el cohete1 llama al método 'accelerar' de la clase 'Cohete'
+    imprime(id); // llama a la función para imprimir por pantalla los datos
+    advertencia(id);
 }
 
-function accelera2() {
-    cohete2.accelerar();
-    imprime2();
-    advertenciaR2();
-}
 
 
 // FRENAR COHETES. Misma dinámica que 'accelerar' pero llamando al métod 'frenar' de la clase 'Cohete'
-function frena1() {
-    cohete1.frenar();
-    imprime1();
-    advertenciaR1();
-}
 
-function frena2() {
-    cohete2.frenar();
-    imprime2();
-    advertenciaR2();
+function frena(id:number) {
+    cohetes[id].frenar();
+    imprime(id);
+    advertencia(id);
 }
 
 
 // MOSTRAR DATOS POR PANTALLA. 
-function imprime1() {
-    let info = document.getElementById("infoR1");
+function imprime(id:number) {
+    let info = document.getElementById("infoR"+id);
     if (info) {
-        info.innerHTML = cohete1.mostrarInfo();
+        info.innerHTML = cohetes[id].mostrarInfo();
     }
 }
 
-function imprime2() {
-    let info = document.getElementById("infoR2");
-    if (info) {
-        info.innerHTML = cohete2.mostrarInfo();
-    }
-}
+
 
 function imprimirTodos() {
     let info = document.getElementById("todos");
@@ -110,16 +96,17 @@ function refreshTodos() {
     let refresh = document.getElementById("todos");
     if (refresh) {
 
-        refresh.innerHTML = cohete1.mostrarInfo() +  "<br><br>" + cohete2.mostrarInfo();
+        refresh.innerHTML = cohete0.mostrarInfo() +  "<br><br>" + cohete1.mostrarInfo();
 
     }
 }
 
 // MOSTRAR AVISOS
-function advertenciaR1() {
-    let mensaje = document.getElementById("avisoR1");
-    let maxima = cohete1.potenciaTotalMaxima();
-    let actual = cohete1.potenciaTotalActual();
+
+function advertencia(id:number) {
+    let mensaje = document.getElementById("avisoR"+id);
+    let maxima = cohetes[id].potenciaTotalMaxima();
+    let actual = cohetes[id].potenciaTotalActual();
     let control = maxima - 20;
     if(mensaje){
         mensaje.classList.remove("send");
@@ -143,32 +130,11 @@ function advertenciaR1() {
     }
 }
 
-function advertenciaR2() {
-    let mensaje = document.getElementById("avisoR2");
-    let maxima = cohete2.potenciaTotalMaxima();
-    let actual = cohete2.potenciaTotalActual();
-    let control = maxima - 20;
-    if(mensaje){
-        mensaje.classList.remove("send");
-        mensaje.classList.remove("stop");
-        mensaje.classList.remove("ready");
-        mensaje.classList.remove("control");
-        if (actual ==  maxima ) {
-            mensaje.innerHTML = "SENDING INTO SPACE!!";
-            mensaje.classList.add("send");
-        } else if(actual >= control){
-            mensaje.innerHTML = "Ready to send";
-            mensaje.classList.add("ready");
 
-        }else if(actual == 0){
-            mensaje.innerHTML = "STOPPED";
-            mensaje.classList.add("stop");
-        }else{
-            mensaje.innerHTML = "Under control";
-            mensaje.classList.add("control");
-        }
-    }
-}
+
+
+
+
 
 
 
